5e78f6e0ea56d51b79fde579607e7b88566f7b13
nathan-n-poon <55016299+nathan-n-poon@users.noreply.github.com>
2023-01-22 23:26:55 -0800

    some more formatting

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,15 +4,24 @@ import './styles.css';
 const AboutMe = () => {
     return (
         <div id={'AboutMeContainer'} >
-            <div id={'Headshot'}>
+            <div id={'headshot'}>
                 <img src={require('../../images/img.png')} alt={'A picture of me'}/>
             </div>
 
             <div id={'description'}>
                 <h1>Hi! My name is Nathan</h1>
-                <h3>Welcome to my website.</h3>
+                <h3>Welcome to my website. Here, I share a bit about me as well as my experiences with computer
+                    engineering.</h3>
+            </div>
+
+            <div id={'details'}>
+                <h3>Some things I'm interested in:</h3>
                 <ul>
-                    <li></li>
+                    <li>Biking</li>
+                    <li>Hiking</li>
+                    <li>Reading novels</li>
+                    <li>Vegan cooking</li>
+
                 </ul>
             </div>
         </div>
