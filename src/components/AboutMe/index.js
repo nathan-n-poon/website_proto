e7db4ad794e3761c92bc758925b9069e7ee09c83
nathan-n-poon <55016299+nathan-n-poon@users.noreply.github.com>
2023-01-25 23:15:49 -0800

    some remodeling

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,28 +3,32 @@ import './styles.css';
 
 const AboutMe = () => {
     return (
-        <div id={'AboutMeContainer'} >
-            <div id={'headshot'}>
-                <img src={require('../../images/img.png')} alt={'A picture of me'}/>
-            </div>
+        <>
+            <div id={'intro'}>
+                <div id={'headshot'}>
+                    <img src={require('../../images/img.png')} alt={'A picture of me'}/>
+                </div>
 
-            <div id={'description'}>
-                <h1>Hi! My name is Nathan</h1>
-                <h3>Welcome to my website. Here, I share a bit about me as well as my experiences with computer
-                    engineering.</h3>
-            </div>
+                <div id={'description'}>
+                    <h1>Hi! My name is Nathan</h1>
+                    <h3>Welcome to my website. Here, I share a bit about me as well as my experiences with computer
+                        engineering.</h3>
+                    <br/>
+                    <h3>Some things I'm interested in:</h3>
+                    <ul>
+                        <li>Biking</li>
+                        <li>Hiking</li>
+                        <li>Reading novels</li>
+                        <li>Vegan cooking</li>
 
-            <div id={'details'}>
-                <h3>Some things I'm interested in:</h3>
-                <ul>
-                    <li>Biking</li>
-                    <li>Hiking</li>
-                    <li>Reading novels</li>
-                    <li>Vegan cooking</li>
+                    </ul>
+                </div>
 
-                </ul>
             </div>
-        </div>
+            <div id={'work_experience'}>
+
+            </div>
+        </>
     );
 }
 
